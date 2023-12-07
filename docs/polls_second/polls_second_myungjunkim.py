cfc00e27ffa21ed926d1ee19d66ed0fc251541da
sweetmjkim <sweet.mjkim@gmail.com>
2023-12-07 11:48:20 +0900

    polls_second_myungjunkim

--- docs/polls_second/polls_second_myungjunkim.py
+++ docs/polls_second/polls_second_myungjunkim.py
@@ -1,5 +1,4 @@
-# source from  
-
+# source from ..polls_first/polls_first_myungjunkim.py
 
 
 list_question = [
@@ -9,9 +8,8 @@ list_question = [
     "상품에 대한 전반적인 만족도는 어떠신가요?"
 ]
 
-
 list_answer =  ["좋음", "중간", "좋아지길"]
-
+list_index_answer =  [0 , 0, 0]
 for num_first in [0,1,2,3]:
     str_question_number = num_first + 1
     str_question = list_question[num_first]
@@ -21,15 +19,25 @@ for num_first in [0,1,2,3]:
         str_answer_number = num_second + 1
         str_answer = list_answer[num_second]
         print("{}. {}".format(str_answer_number, str_answer), end = " ")
-        
         pass
-  
-    if num_first < 3:
-        print("\n-----------------------------------------\n")
-        pass
-   
+
+
+    if num_first <= 3:
+        print("\n")
+        str_question_result = input("당신 생각은 몇번 : ")
+        num_question_result = int(str_question_result) # 문자를 숫자로 변환
+        index = num_question_result - 1 # index 위치값 적용
+
+
+        print("-----------------------------------------")
+    list_index_answer[index] = list_index_answer[index] + 1
     pass
+
+
+print("")
+print("---------------- 통 계 ----------------")
+print("설문자 답항별 갯수 표시 : {} ".format(list_index_answer))
 print("\n")
 
-
-    
+weighted_mean = ((list_index_answer[0] * 3) + (list_index_answer[1] * 2) +(list_index_answer[2] * 1)) / (list_index_answer[0] + list_index_answer[1] + list_index_answer[2])
+print(weighted_mean)
