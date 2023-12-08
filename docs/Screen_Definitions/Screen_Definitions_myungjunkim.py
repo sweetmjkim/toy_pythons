1a5e56753d65af9abe99d675d1d106ab39d90f47
sweetmjkim <sweet.mjkim@gmail.com>
2023-12-08 17:49:21 +0900

    add

--- docs/Screen_Definitions/Screen_Definitions_myungjunkim.py
+++ docs/Screen_Definitions/Screen_Definitions_myungjunkim.py
@@ -3,45 +3,32 @@
 # 업무 분장 : 문제 풀고 / 점수 따른 학점 출력
 # 특정 시점에 통합 : 각자 만든 코드 합함.
 
-# # 문제 만들기 _ juuuuuu
-# 아래는 4개의 Python 관련 문제와 각 문항의 난이도에 따른 점수화
-# 1. 문제: Python에서 변수를 선언하는 방법은? (점수: 10점)
-#    1) var name, 2) name = value, 3) set name, 4) name == value
-#    - 정답: 2
-# 2. 문제: Python에서 리스트(List)의 특징은 무엇인가요? (점수: 15점)
-#    1) 순서가 있고 변경 가능하다, 2) 중복된 값을 가질 수 없다, 3) 원소를 추가하거나 삭제할 수 없다, 4) 정렬된 상태로 유지된다
-#    - 정답: 1
-# 3. 문제: Python에서 조건문을 작성하는 방법은? (점수: 10점)
-#    1) if-else, 2) for-in, 3) while, 4) def
-#    - 정답: 1
-# 4. 문제: Python에서 함수를 정의하는 방법은? (점수: 5점)
-#    1) class, 2) def, 3) import, 4) return
-#    - 정답: 2
+list_problems = ["1. 문제: Python에서 변수를 선언하는 방법은? (점수: 10점)"
+            ,"1) var name, 2) name = value, 3) set name, 4) name == value"
+            ,"2. 문제: Python에서 리스트(List)의 특징은 무엇인가요? (점수: 15점)"
+            ,"1) 순서가 있고 변경 가능하다, 2) 중복된 값을 가질 수 없다, 3) 원소를 추가하거나 삭제할 수 없다, 4) 정렬된 상태로 유지된다"
+            ,"3. 문제: Python에서 조건문을 작성하는 방법은? (점수: 10점)"
+            ,"1) if-else, 2) for-in, 3) while, 4) def"
+            ,"4. 문제: Python에서 함수를 정의하는 방법은? (점수: 5점)"
+            ,"1) class, 2) def, 3) import, 4) return"
+              ]
+list_anwser = [0, 1, 2, 3]
+# tmp_anwser = [1, 2, 3, 4]
+for num_count in [0, 2, 4, 6] :
+    str_question = list_problems[num_count]
+    print("{}".format(str_question))
+    str_anwser = list_problems[num_count+1]
+    print("{}".format(str_anwser))
 
-# —----- 결과 —-------------
-# 응답한 내용 : 1번 2, 2번 1, 3번 1, 4번 2
-# 당신 응답 합계 : 40점
-# 학점은 A 입니다.
-# # 
+    # str_question_result = input("당신의 답변(순서 맞게 번호로 입력) : ")
+    # num_question_result = int(str_question_result) # 문자를 숫자로 변환
+    num_question_result = 1 # 문자를 숫자로 변환
+    index = num_question_result - 1 # index 위치값 적용
+    
+    # list_anwser[index]  = list_anwser[index] + 1 
 
-def question():    
-    question_list = ["1. 문제: Python에서 변수를 선언하는 방법은? (점수: 10점)",
-                    "2. 문제: Python에서 리스트(List)의 특징은 무엇인가요? (점수: 15점)",
-                    "3. 문제: Python에서 조건문을 작성하는 방법은? (점수: 10점)",
-                    "4. 문제: Python에서 함수를 정의하는 방법은? (점수: 5점)"
-                    ]
-    answer_list = ["1) var name, 2) name = value, 3) set name, 4) name == value",
-                "1) 순서가 있고 변경 가능하다, 2) 중복된 값을 가질 수 없다, 3) 원소를 추가하거나 삭제할 수 없다, 4) 정렬된 상태로 유지된다",
-                "1) if-else, 2) for-in, 3) while, 4) def",
-                "1) class, 2) def, 3) import, 4) return"
-    ]
-
-    user_answer = []
-    for a in range(len(question_list)):
-        print(question_list[a])
-        print(answer_list[a])
-        input_answer = int(input("- 정답:"))
-        user_answer.append(input_answer)
-        pass
+    print("------------------------------------------")
     pass
-    return user_answer
+print("선호 답항 : {}" .format(list_anwser))
+print("End Program!")
+
