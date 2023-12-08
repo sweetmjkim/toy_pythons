31d89487de1d4696fe3269489b03bda571bce39c
sweetmjkim <sweet.mjkim@gmail.com>
2023-12-08 14:07:19 +0900

    Screen Definitions_myungjunkim

---
+++ docs/Screen Definitions/.py
@@ -0,0 +1,23 @@
+list_problems = ["1. 문제: Python에서 변수를 선언하는 방법은? (점수: 10점)"
+            ,"1) var name, 2) name = value, 3) set name, 4) name == value"
+            ,"2. 문제: Python에서 리스트(List)의 특징은 무엇인가요? (점수: 15점)"
+            ,"1) 순서가 있고 변경 가능하다, 2) 중복된 값을 가질 수 없다, 3) 원소를 추가하거나 삭제할 수 없다, 4) 정렬된 상태로 유지된다"
+            ,"3. 문제: Python에서 조건문을 작성하는 방법은? (점수: 10점)"
+            ,"1) if-else, 2) for-in, 3) while, 4) def"
+            ,"4. 문제: Python에서 함수를 정의하는 방법은? (점수: 5점)"
+            ,"1) class, 2) def, 3) import, 4) return"
+            ]
+
+list_ex = ["1. 문제: Python에서 변수를 선언하는 방법은? (점수: 10점)"
+            ,"1) var name, 2) name = value, 3) set name, 4) name == value"
+            ,"2. 문제: Python에서 리스트(List)의 특징은 무엇인가요? (점수: 15점)"
+            ,"1) 순서가 있고 변경 가능하다, 2) 중복된 값을 가질 수 없다, 3) 원소를 추가하거나 삭제할 수 없다, 4) 정렬된 상태로 유지된다"
+            ,"3. 문제: Python에서 조건문을 작성하는 방법은? (점수: 10점)"
+            ,"1) if-else, 2) for-in, 3) while, 4) def"
+            ,"4. 문제: Python에서 함수를 정의하는 방법은? (점수: 5점)"
+            ,"1) class, 2) def, 3) import, 4) return"
+            ]
+
+
+for num_count in [] :
+    print(list_problems())
