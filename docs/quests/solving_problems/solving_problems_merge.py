bc2c664334fd1d1ddd04a2dcc1c2cf8fa434c7c2
scvgong <gongmyeongyun@gmail.com>
2023-12-11 14:50:01 +0900

    add merge

--- docs/quests/solving_problems/solving_problems_merge.py
+++ docs/quests/solving_problems/solving_problems_merge.py
@@ -1,44 +1,29 @@
-def question():    
-    question_list = ["1. 문제: Python에서 변수를 선언하는 방법은? (점수: 10점)",
-                    "2. 문제: Python에서 리스트(List)의 특징은 무엇인가요? (점수: 15점)",
-                    "3. 문제: Python에서 조건문을 작성하는 방법은? (점수: 10점)",
-                    "4. 문제: Python에서 함수를 정의하는 방법은? (점수: 5점)"
-                    ]
-    answer_list = ["1) var name, 2) name = value, 3) set name, 4) name == value",
-                "1) 순서가 있고 변경 가능하다, 2) 중복된 값을 가질 수 없다, 3) 원소를 추가하거나 삭제할 수 없다, 4) 정렬된 상태로 유지된다",
-                "1) if-else, 2) for-in, 3) while, 4) def",
-                "1) class, 2) def, 3) import, 4) return"
-    ]
+def cal_grade():
 
-    user_answer = []
-    for a in range(len(question_list)):
-        print(question_list[a])
-        print(answer_list[a])
-        input_answer = int(input("- 정답:"))
-        user_answer.append(input_answer)
-        pass
-    pass
-    return user_answer
+    user_answer = [0,0,0,0]     # 입력 받을 정답 변수 
+    list_score = [10,15,10,5]   # 정답에 따른 점수 변수
+    score = 0                   # 합산 변수
 
-def result_cal(user_answer) :
-    pass
-    correct_answer = [2,1,1,2]
-    score_list=[10,15,10,5]
-    score=[]
     for i in range(len(user_answer)):
-       if user_answer[i] == correct_answer[i] :
-            score.append(int(score_list[i]))
-            user_sum = sum(score)
-    if user_sum >=30 :
-        user_score = "A"
-    elif user_sum >=20 :
-        user_score = "B"
-    else:
-        user_score = "C"
-    print ("--------결과---------")
-    print("응답한 내용 : 1번 {}, 2번 {}, 3번 {}, 4번 {}".format(user_answer[0], user_answer[1],user_answer[2], user_answer[3]))
-    print("당신 응답 합계 : {}점".format(user_sum))
-    print("학점은 {}입니다.".format(user_score))
- 
+        input_answer = int(input("정답 입력 : ")) # 정답 입력받을 변수
+        user_answer[i] = input_answer # user_answer에 정답 삽입
 
-result_cal(question())
+        # 정답 입력 번호 시 점수 합산
+        if input_answer == 2:
+            score += list_score[i]
+        elif input_answer == 1:
+            score += list_score[i]
+
+        # 등급 매기기
+        if score >= 30:
+            user_grade = 'A'
+        elif score >= 20:
+            user_grade = 'B'
+        else :
+            user_grade = 'C'
+
+    print("응답결과 : 1번 : {} 2번 : {}, 3번 : {}, 4번 : {}".format(user_answer[0],user_answer[1],user_answer[2],user_answer[3]))
+    print("합산 점수 : {}".format(score))
+    print("등급 : {}".format(user_grade))
+        
+cal_grade()
