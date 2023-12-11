446d8275296af6fa762aff21a6c9c179d55b05b9
scvgong <gongmyeongyun@gmail.com>
2023-12-11 16:19:04 +0900

    move file

--- docs/solving_problems_classes/solving_problems_merge.py
+++ docs/solving_problems_classes/solving_problems_merge.py
@@ -1,41 +1,51 @@
-# 입력한 값으로 점수 합계(진행중)
-def cal_score():
+# 클래스화 작업
+class QuizTest:
+    def __init__(self):
+        self.question_list = ["1. 문제: Python에서 변수를 선언하는 방법은? (점수: 10점)",
+                              "2. 문제: Python에서 리스트(List)의 특징은 무엇인가요? (점수: 15점)",
+                              "3. 문제: Python에서 조건문을 작성하는 방법은? (점수: 10점)",
+                              "4. 문제: Python에서 함수를 정의하는 방법은? (점수: 5점)"
+                             ]
+        self.answer_list = ["1) var name, 2) name = value, 3) set name, 4) name == value",
+                            "1) 순서가 있고 변경 가능하다, 2) 중복된 값을 가질 수 없다, 3) 원소를 추가하거나 삭제할 수 없다, 4) 정렬된 상태로 유지된다",
+                            "1) if-else, 2) for-in, 3) while, 4) def",
+                            "1) class, 2) def, 3) import, 4) return"
+        ]
+        self.correct_answer = [2,1,1,2]
+        self.score_list=[10,15,10,5]
 
-    user_answer = [0,0,0,0]     # 입력 받을 정답 변수 
-    list_score = [10,15,10,5]   # 정답에 따른 점수 변수
-    score = 0                   # 합산 변수
+    def question(self):
+        self.user_answer = []
 
-    for i in range(len(user_answer)):
-        input_answer = int(input("정답 입력 : ")) # 정답 입력받을 변수
-        user_answer[i] = input_answer # user_answer에 정답 삽입
+        for a in range(len(self.question_list)): 
+            print(self.question_list[a])
+            print(self.answer_list[a])
+            input_answer = int(input("- 정답:"))
+            self.user_answer.append(input_answer)
 
-        # 정답 입력 번호 시 점수 합산
-        if input_answer == 2:
-            score += list_score[i]
-        elif input_answer == 1:
-            score += list_score[i]
+        return self.user_answer
 
-    return user_answer,score
+    def result_cal(self):
+        score=[]
+        for i in range(len(self.user_answer)):
+            if self.user_answer[i] == self.correct_answer[i] :
+                score.append(int(self.score_list[i]))
+                user_sum = sum(score)
 
-def main(user_answer, score): # 등급 매기고 출력
-    # 등급 매기기
-    if score >= 30:
-        user_grade = 'A'
-    elif score >= 20:
-        user_grade = 'B'
-    else :
-        user_grade = 'C'
-        
-    print("응답결과 : 1번 : {} 2번 : {}, 3번 : {}, 4번 : {}".format(user_answer[0],user_answer[1],user_answer[2],user_answer[3]))
-    print("합산 점수 : {}".format(score))
-    print("등급 : {}".format(user_grade))
+        if user_sum >=30 :
+            user_score = "A"
+        elif user_sum >=20 :
+            user_score = "B"
+        else:
+            user_score = "C"
+        print ("--------결과---------")
+        print("응답한 내용 : 1번 {}, 2번 {}, 3번 {}, 4번 {}".format(self.user_answer[0], self.user_answer[1],self.user_answer[2], self.user_answer[3]))
+        print("당신 응답 합계 : {}점".format(user_sum))
+        print("학점은 {}입니다.".format(user_score))
 
+# 클래스의 인스턴스 생성
+quiz = QuizTest()
 
-user_answer, score = cal_score()
-main(user_answer,score)
-
-
-
-# calculate = Calculate()
-# calculate.cal_score(user_answer, score)
-# calculate.main(user_answer, score)
+# 메소드 호출
+quiz.question()
+quiz.result_cal()
